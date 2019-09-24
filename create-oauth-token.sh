ca6fa94e7f43cf4251bab3f4754a00b0eccf8432
Lauri Piispanen <lauri.piispanen@reaktor.com>
2019-09-24 22:46:23 +0300

    reauth and revoke credentials when done

--- create-oauth-token.sh
+++ create-oauth-token.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-TOKEN=$(gcloud auth print-access-token)
-ACCOUNT_EMAIL_OR_UNIQUEID=$1
-
-
 if [[ -z "$1" ]]
 then
   echo "Please provide service account email or unique id"
@@ -11,12 +7,18 @@ then
   echo "To see which service accounts are available: gcloud iam service-accounts list"
   exit 1
 else
+  gcloud auth login --no-user-output-enabled --verbosity=none
+
+  TOKEN=$(gcloud auth print-access-token)
+  ACCOUNT_EMAIL_OR_UNIQUEID=$1
   access_token_regex='"accessToken": "([^"]*)'
 
   lifetime=${2:-300s}
-  response=$(curl -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
+  response=$(curl -s -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
 	-d "{\"delegates\": [], \"scope\": [ \"https://www.googleapis.com/auth/cloud-platform\"], \"lifetime\":\"$lifetime\"}" \
 	"https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/$ACCOUNT_EMAIL_OR_UNIQUEID:generateAccessToken")
+
+  gcloud auth revoke --no-user-output-enabled
 
   if [[ $response =~ $access_token_regex ]]
   then
